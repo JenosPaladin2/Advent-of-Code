8c99cdcb3b892221f75e4dcefa679510d55c4788
JenosPaladin2 <102378548+JenosPaladin2@users.noreply.github.com>
2022-12-25 22:22:02 +0100

    finaler Commit

--- Task2/RockPaperScissors.js
+++ Task2/RockPaperScissors.js
@@ -6,124 +6,153 @@ let relativePathFirstTask = "./SecondTask.txt";
 let data = fs.readFileSync(relativePathFirstTask, "utf8");
 let rockPaperScissorInput = createArrayFromString(data);
 
-//console.log(rockPaperScissorInput);
+console.log(rockPaperScissorInput);
 
 let result = processRockPaperScissorInput(rockPaperScissorInput);
 
-console.log('Enemy score: '+result.enemyScore);
-console.log('My score: '+ result.myScore);
-
-/*
-Vorüberlegung:
-1. Input soll getrennt werden nach Zeilenumbrüchen
-2. Input soll getrennt werden nach Leerzeichen und in zwei Arrays gepackt werden
-3. Array 1 kopieren, kopierte Array soll folgende Ersetzung stattfinden:
-A = 1, B = 2, C=3 UND dann per Schleife die Summe bilden
-4. Array 2 kopieren, kopierte Array soll folgende Ersetzung stattfinden:
-X = 1, Y = 2, Z=3 UND dann per Schleife die Summe bilden
-5. Array1 mit Array2 vergleichen. Wenn linke Seite gewinnt, bei linke Seite dazuaddieren
-Wenn rechte Seite gewinnt, dann bei rechten Seite dazuaddieren.
-*/
-
+console.log("Enemy score: " + result.enemyScore);
+console.log("My score: " + result.myScore);
 
 function createArrayFromString(text) {
-    return text.split(/\r?\n/);
+  return text.split(/\r?\n/);
+}
+
+function processRockPaperScissorInput(table) {
+  let gameProperties = {
+    enemyScore: 0,
+    myScore: 0,
+  };
+
+  let enemyScore = 0;
+  let myScore = 0;
+
+  let gameWithNewStragety = applyWinStrategy(table);
+  console.log(gameWithNewStragety);
+
+  for (let i = 0; i < gameWithNewStragety.length; i++) {
+    gameProperties = distributePointsBasedOnGame(gameWithNewStragety[i]);
+    enemyScore += gameProperties.enemyScore;
+    myScore += gameProperties.myScore;
   }
 
-  function processRockPaperScissorInput(table){
-    
-    let gameProperties = {
-      enemyScore: 0,
-      myScore: 0,
-    }
+  gameProperties.enemyScore = enemyScore;
+  gameProperties.myScore = myScore;
 
-    let enemyScore = 0;
-    let myScore = 0;
+  return gameProperties;
+}
 
-    let gameWithNewStragety = applyWinStrategy(table);
+function distributePointsBasedOnGame(game) {
+  let decisions = [
+    "A X",
+    "A Y",
+    "A Z",
+    "B X",
+    "B Y",
+    "B Z",
+    "C X",
+    "C Y",
+    "C Z",
+  ];
+  //Rock +1, Paper +2, Scissor +3
+  let enemyScoreTable = [4, 1, 7, 8, 5, 2, 3, 9, 6];
+  let myScoreTable = [4, 8, 3, 1, 5, 9, 7, 2, 6];
 
-    for(let i=0; i< gameWithNewStragety.length; i++){
-      gameProperties = distributePointsBasedOnGame(gameWithNewStragety[i]);
-      enemyScore += gameProperties.enemyScore;
-      myScore += gameProperties.myScore;
-    }
+  let decisionsIndex = decisions.indexOf(game);
+  let enemyScore = enemyScoreTable[decisionsIndex];
+  let myScore = myScoreTable[decisionsIndex];
 
-    gameProperties.enemyScore = enemyScore;
-    gameProperties.myScore = myScore;
+  return {
+    enemyScore: enemyScore,
+    myScore: myScore,
+  };
+}
 
-    return gameProperties;
+//second half of the puzzle
+function applyWinStrategy(gameDecisions) {
+  let newGameDecisions = [];
+  let gameDecision = "";
+
+  for (let i = 0; i < gameDecisions.length; i++) {
+    gameDecision = gameDecisions[i];
+    newGameDecisions[i] = chooseRockPaperScissorBasedOnEnemyInput(gameDecision);
   }
 
-  function distributePointsBasedOnGame(game){
-    let decisions = ['A X', 'A Y', 'A Z', 'B X', 'B Y', 'B Z', 'C X', 'C Y', 'C Z'];
-    //Rock +1, Paper +2, Scissor +3
-    let enemyScoreTable = [4,1,7,8,5,2,3,9,6];
-    let myScoreTable = [4,8,3,1,5,9,7,2,6];
+  return newGameDecisions;
+}
 
-    let decisionsIndex = decisions.indexOf(game);
-    let enemyScore = enemyScoreTable[decisionsIndex];
-    let myScore = myScoreTable[decisionsIndex];
-
-    return {
-      enemyScore: enemyScore,
-      myScore: myScore
-    }
-  }
-
-  //second half of the puzzle
-  function applyWinStrategy(gameDecisions){
-    let newGameDecisions = [];
-    let gameDecision = "";
-
-    for(let i = 0; i < gameDecisions.length; i++){
-      gameDecision = gameDecisions[i];
-      newGameDecisions[i] = chooseRockPaperScissorBasedOnEnemyInput(gameDecision);
-    }
-
-    return newGameDecisions;
-  }
-
-function chooseRockPaperScissorBasedOnEnemyInput(gameDecision){
+function chooseRockPaperScissorBasedOnEnemyInput(gameDecision) {
   let newDecision = "";
 
-  if(gameDecision.includes('A')){
-    newDecision = chooseRockOnEnemyRock(gameDecision);
-  }else if(gameDecision.includes('B')){
-    newDecision = choosePaperOnEnemyPaper(gameDecision);
-  }else if(gameDecision.includes('C')){
-    newDecision = chooseRockOnEnemyScissor(gameDecision);
+  if (gameDecision.includes("X")) {
+    newDecision = getLoose(gameDecision);
+  } else if (gameDecision.includes("Y")) {
+    newDecision = getDraw(gameDecision);
+  } else if (gameDecision.includes("Z")) {
+    newDecision = getWin(gameDecision);
   }
 
   return newDecision;
 }
 
-function chooseRockOnEnemyRock(gameDecision){
-  let regEx = /[Y,Z]/g
-    return gameDecision.replace(regEx,"X");
+function getDraw(gameDecision) {
+  let newGameDecision = "";
+  let regEx = /[Y,Z]/g;
+
+  if (gameDecision.includes("A")) {
+    newGameDecision = gameDecision.replace(regEx, "X");
+  } else if (gameDecision.includes("B")) {
+    regEx = /[X,Z]/g;
+    newGameDecision = gameDecision.replace(regEx, "Y");
+  } else if (gameDecision.includes("C")) {
+    regEx = /[X,Y]/g;
+    newGameDecision = gameDecision.replace(regEx, "Z");
+  }
+  return newGameDecision;
 }
 
-function choosePaperOnEnemyPaper(gameDecision){
-  let regEx = /[X,Z]/g
-    return gameDecision.replace(regEx,"Y");
+function getLoose(gameDecision) {
+  let newGameDecision = "";
+  let regEx = /[X,Y]/g;
+
+  if (gameDecision.includes("A")) {
+    newGameDecision = gameDecision.replace(regEx, "Z");
+  } else if (gameDecision.includes("B")) {
+    regEx = /[Y,Z]/g;
+    newGameDecision = gameDecision.replace(regEx, "X");
+  } else if (gameDecision.includes("C")) {
+    regEx = /[X,Z]/g;
+    newGameDecision = gameDecision.replace(regEx, "Y");
+  }
+  return newGameDecision;
 }
 
-function chooseRockOnEnemyScissor(gameDecision){
-  let regEx = /[Y,Z]/g
-    return gameDecision.replace(regEx,"X");
+function getWin(gameDecision) {
+  let newGameDecision = "";
+  let regEx = /[X,Z]/g;
+
+  if (gameDecision.includes("A")) {
+    newGameDecision = gameDecision.replace(regEx, "Y");
+  } else if (gameDecision.includes("B")) {
+    regEx = /[X,Y]/g;
+    newGameDecision = gameDecision.replace(regEx, "Z");
+  } else if (gameDecision.includes("C")) {
+    regEx = /[Y,Z]/g;
+    newGameDecision = gameDecision.replace(regEx, "X");
+  }
+  return newGameDecision;
 }
 
 function hasEmptyValueOnIndex(array, number) {
-    return array[number] == "";
+  return array[number] == "";
+}
+
+function convertStringIntoNumberArray(array) {
+  let newArray = [];
+  let value = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    value = !hasEmptyValueOnIndex(array, i) ? parseInt(array[i]) : array[i];
+    newArray.push(value);
   }
-  
-  function convertStringIntoNumberArray(array) {
-    let newArray = [];
-    let value = 0;
-  
-    for (let i = 0; i < array.length; i++) {
-      value = !hasEmptyValueOnIndex(array, i) ? parseInt(array[i]) : array[i];
-      newArray.push(value);
-    }
-    return newArray;
-  }
-  
+  return newArray;
+}
